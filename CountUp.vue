2942a1f9d8f65ee41105f38b78b383ff7724e2b5
yangyuhang <417165978@qq.com>
2020-04-26 11:32:44 +0800

    第一次提交组件到githun

---
+++ CountUp.vue
@@ -0,0 +1,56 @@
+<template>
+  <span class="iCountUp">
+    <ICountUp
+      :delay="delay"
+      :endVal="endVal"
+      :options="options"
+      @ready="onReady"
+    />
+  </span>
+</template>
+
+<script type="text/babel">
+import ICountUp from "vue-countup-v2";
+export default {
+  name: "demo",
+  components: {
+    ICountUp
+  },
+  data() {
+    return {
+      options: {
+        useEasing: true,
+        useGrouping: true,
+        separator: ",",
+        decimal: ".",
+        prefix: "",
+        suffix: ""
+      }
+    };
+  },
+  props: {
+    endVal: {
+      type: Number,
+      default: 30000
+    },
+    delay: {
+      type: Number,
+      default: 1000
+    }
+  },
+  methods: {
+    onReady: function(instance, CountUp) {
+      const that = this;
+      instance.update(that.endVal);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.iCountUp {
+  font-size: 80px;
+  margin: 0;
+  color: #4d63bc;
+}
+</style>
